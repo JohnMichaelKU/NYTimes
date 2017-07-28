19e7d774b934d9c7ad2d6b3bac9f889f3baee9da
JohnMichaelKU <Johnmichaelmcgrew@gmail.com>
2017-07-27 21:20:40 -0500

    added javascript

---
+++ api.js
@@ -0,0 +1,28 @@
+$("#submit-button").click(function() {
+	var querySearch = $("#search-term").val().trim();
+	var recordsDisplayed = $("#records-to-retrieve").val();
+	var startYear = $("#start-year").val().trim();
+	var endYear = $("#end-year").val().trim()
+	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+	url += '?' + $.param({
+  		'api-key': "3a960e275b2442f99d20ec7491243dff",
+  		'q' : querySearch,
+  		'begin_date' : startYear,
+  		'end_date' : endYear,
+	});
+	$.ajax({
+  		url: url,
+  		method: 'GET',
+	}).done(function(data) {
+  		console.log(data);
+  		for(i = 0; i < recordsDisplayed; i++) {
+  			$("#articles-form").append('<a>' + data.response.docs[i].web_url + '</a>')
+  			.append('<br>' + data.response.docs[i].lead_paragraph)
+  			.append('<br><br>')
+  		}
+
+	}).fail(function(err) {
+  		throw err;
+	});
+});
+
